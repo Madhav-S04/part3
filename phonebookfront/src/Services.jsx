3753c7a2ca0e37d078855dac98948c9506d571d9
Madhav-S04 <madhavpattambi@gmail.com>
2025-03-01 23:05:12 +0530

    modified:   phonebookfront/src/Services.jsx

--- phonebookfront/src/Services.jsx
+++ phonebookfront/src/Services.jsx
@@ -4,6 +4,7 @@ import axios from 'axios';
 const baseUrl = '/api/persons'; // Relative path
 
 //const baseUrl = 'http://localhost:3001/api/persons'; // Update to backend URL
+//const baseUrl = import.meta.env.VITE_BACKEND_URL || '/api/persons';
 
 const getAll = () => axios.get(baseUrl).then(response => response.data);
 
@@ -13,5 +14,4 @@ const remove = id => axios.delete(`${baseUrl}/${id}`).then(response => response.
 
 export default { getAll, create, remove };
 
-//const baseUrl = import.meta.env.VITE_BACKEND_URL || '/api/persons';
 //const baseUrl =  '/api/persons';
