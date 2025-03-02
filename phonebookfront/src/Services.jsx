3a076cceacb55ab83941b55c65d02edf52d4b3d8
Madhav-S04 <madhavpattambi@gmail.com>
2025-03-02 21:56:55 +0530

    modified:   phonebookfront/src/Services.jsx

--- phonebookfront/src/Services.jsx
+++ phonebookfront/src/Services.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
-//const baseUrl = 'https://part3-er3c.onrender.com/api/persons'; 
-const baseUrl = '/api/persons'; // Relative path
+const baseUrl = 'https://part3-er3c.onrender.com/api/persons'; 
+//const baseUrl = '/api/persons'; // Relative path
 
 //const baseUrl = 'http://localhost:3001/api/persons'; // Update to backend URL
 //const baseUrl = import.meta.env.VITE_BACKEND_URL || '/api/persons';
